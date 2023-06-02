90aaeac8bc2c0ec7e288ac0abda3fc772ab63acb
seherzengin <seher.zengin@atos.net>
2023-06-02 15:39:19 +0300

    first commit

---
+++ movieapp/src/app/category/category.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../models/category';
+import { CategoryRepository } from '../models/category.repository';
+import { CategoryService } from '../services/category.service';
+//import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css'],
+  providers: [CategoryService]
+})
+export class CategoryComponent implements OnInit {
+
+  categories: Category[] = [];
+  selectedCategory: any =null;
+
+  constructor(private categoryService:CategoryService) {
+  }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data;
+    })
+  }
+
+  displayAll = true;
+
+  selectCategory(item? : Category) {
+      if(item){
+        this.selectedCategory = item;
+        this.displayAll = false;
+      }else{
+        this.selectedCategory = null;
+        this.displayAll = true;
+      }
+  }
+
+}
